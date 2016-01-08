170b2c1a1bbdf01ed04c9bdd9271e46fef2c0d29
tinybearc <tinybearc@gmail.com>
2016-01-08 22:34:35 +0800

    cleanup used files

--- lua/frameworks/runtime-src/proj.android/jars/facebook_lib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/private/var/jenkins/.jenkins/jobs/_release_1.5.2/workspace/plugins/facebook/proj.android/libs/facebook_lib/bin/AndroidManifest.xml : \
-/private/var/jenkins/.jenkins/jobs/_release_1.5.2/workspace/plugins/facebook/proj.android/libs/facebook_lib/AndroidManifest.xml \
