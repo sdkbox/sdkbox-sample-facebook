5df995842a734751917247086905fe4b214bd98f
yinjimmy <jimmy.yin5@gmail.com>
2016-05-11 14:57:58 +0800

    make compile error

--- cpp/Classes/HelloWorldScene.cpp
+++ cpp/Classes/HelloWorldScene.cpp
@@ -2,7 +2,7 @@
 #include "HelloWorldScene.h"
 #include "PluginFacebook/PluginFacebook.h"
 
-#define PICOJSON_USE_LOCALE 0
+#define PICOJSON_USE_LOCALE 1
 #include "picojson.h"
 #include "network/HttpRequest.h"
 #include "network/HttpClient.h"
